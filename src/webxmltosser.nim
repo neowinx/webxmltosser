c2c75d0a7d8e34396f543b292b9a0be5f0431fb3
Pedro Flores <pflores@dncp.gov.py>
2019-07-24 17:13:06 -0400

    this is now working apparently

--- src/webxmltosser.nim
+++ src/webxmltosser.nim
@@ -1,4 +1,4 @@
-import os, streams, xmlparser, xmltree, strutils, strformat
+import os, streams, xmlparser, xmltree, strutils, strformat, sequtils
 
 when isMainModule:
 
@@ -6,5 +6,13 @@ when isMainModule:
     quit("Usage: webxmltosser init-param name value web.xml")
   
   var x = loadXml(paramStr(3))
-  var initParams = x.findAll("init-param")
-  echo fmt"{initParams}"
+  let params = concat(x.findAll("context-param"), x.findAll("init-param"))
+  for ip in params:
+    let pns = ip.findAll("param-name")
+    for pn in pns:
+      if pn.innerText == paramStr(1):
+        let pvs = ip.findAll("param-value")
+        for pv in pvs:
+          pv[0].text = paramStr(2)
+
+  echo fmt"{x}"
